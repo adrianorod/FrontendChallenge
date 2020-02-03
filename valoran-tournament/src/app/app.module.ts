65eb55dc7619796db1da5253e4b148ca7c8a5d0f
adrianorod <adrianoazevedo.r@gmail.com>
2020-02-03 01:18:46 -0300

    feat: atualiza módulos

--- valoran-tournament/src/app/app.module.ts
+++ valoran-tournament/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { CreateTournamentComponent } from './pages/create-tournament/create-tournament.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TournamentComponent } from './pages/tournament/tournament.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CreateTournamentComponent,
+    TournamentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
