2550e92a70c46971bb5d59a6bf6efb00b5087814
adrianorod <adrianoazevedo.r@gmail.com>
2020-02-03 02:09:30 -0300

    feat: cria pagina de torneio

--- valoran-tournament/src/app/app-routing.module.ts
+++ valoran-tournament/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { CreateTournamentComponent } from './pages/create-tournament/create-tournament.component';
+import { TournamentComponent } from './pages/tournament/tournament.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create-tournament', component: CreateTournamentComponent },
+  { path: 'tournament', component: TournamentComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
--- valoran-tournament/src/app/pages/tournament/tournament.component.ts
+++ valoran-tournament/src/app/pages/tournament/tournament.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PlayersStorageService } from 'src/app/services/players-storage.service';
 
 @Component({
   selector: 'app-tournament',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TournamentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private playersService: PlayersStorageService, private router: Router) { }
+
+  players: object;
+  tournament: Array<string>;
 
   ngOnInit() {
+    this.players = this.playersService.getPlayers();
+
+    if (this.players) {
+      this._mountPlayers();
+    } else {
+      this.router.navigate(['']);
+    }
   }
 
+  private _mountPlayers() {
+    this.tournament = Object.entries(this.players).map((item) => item[1]);
+  }
+
+  startTournament() {
+    if (this.tournament.length >= 2) {
+      const totalPlayers = this.tournament.length;
+      const reduceTo = totalPlayers / 2;
+
+      for (let i = 1; reduceTo >= i; i++) {
+        const remove = Math.floor(Math.random() * (totalPlayers - 1));
+        this.tournament = this.tournament.filter((item) => item !== this.tournament[remove]);
+      }
+    }
+  }
+
+  reset() {
+    this.router.navigate(['']);
+  }
 }
